46a40cdf27401d8e02ced0f9bf91e1421e3b7230
kunkka <110076790@qq.com>
2026-03-09 08:35:53 +0800

    chore: remove all one-off scripts, logs, and debug files

--- scripts/debug2.mjs
+++
@@ -1,7 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs'
-const c = readFileSync('e:/webapp/programtools/src/pages/Design.vue', 'utf8')
-const idx = c.indexOf('embedded-exit-bar')
-console.log('exit-bar at:', idx)
-if (idx >= 0) {
-  console.log(JSON.stringify(c.substring(idx-10, idx+300)))
-}
