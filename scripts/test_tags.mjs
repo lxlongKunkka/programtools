2998b431f4bbce3d5a2e7589fce2910929cc57df
kunkka <110076790@qq.com>
2026-03-14 11:27:38 +0800

    deploy 2026-3-14 11-27-37

--- scripts/test_tags.mjs
+++
@@ -1,3 +0,0 @@
-import { fetchHydroNflsoiContest } from '/var/www/programtools/server/routes/hydro_nflsoi.js'
-const r = await fetchHydroNflsoiContest('http://nflsoi.cc:10611/contest/69ad79c783d6583e0f6d26cd/problems')
-r.problems.slice(0, 3).forEach(p => console.log(p.label, p.title, 'tags:', JSON.stringify(p.tags)))
