eb9a0629dc0849c62ffaec623d5c2e6c96230e4c
unknown <you@example.com>
2026-06-03 16:13:09 +0800

    deploy 2026-6-3 16-13-09

--- src/pages/GraphEditor.vue
+++ src/pages/GraphEditor.vue
@@ -157,7 +157,7 @@ export default {
         nodes: {
           shape: 'circle',
           size: 25,
-          font: { size: 16, color: '#fff', bold: true },
+          font: { size: 16, color: '#fff', bold: { enabled: true } },
           color: { background: '#3b82f6', border: '#2563eb', highlight: { background: '#60a5fa', border: '#3b82f6' } }
         },
         edges: {
